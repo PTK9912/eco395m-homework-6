bb2113621611074a7c8c39282fc014914441da54
pang peter <ptk9912@utexas.edu>
2022-03-03 20:19:24 -0600

    Finished

--- code/pandas_election_2020.py
+++ code/pandas_election_2020.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 import os
 import pandas as pd
 
@@ -6,4 +7,12 @@ OUTPUT_DIR = "artifacts"
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "election_report_pandas.csv")
 
 if __name__ == "__main__":
-    # you write this
+    df = pd.read_csv(IN_PATH)
+    
+    
+    df1=df.loc[df.year==2020]
+    df2=df1.groupby(by=["year","state_po","candidate"])["candidatevotes"].sum().reset_index()
+    df3=df2.sort_values(by=["state_po","candidatevotes"],ascending=[True,False])
+    df3['candidatevotes'] = df3['candidatevotes'].astype(int)
+    df3.rename(columns={"state_po":"state_code","candidatevotes":"votes"},inplace=True)
+    df3.to_csv(OUTPUT_PATH,index=False)
